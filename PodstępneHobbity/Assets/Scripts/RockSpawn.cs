34fc980d5c5154b0b76dfef6f82cf1c3aadc64d4
piechu007 <55602442+piechu007@users.noreply.github.com>
2019-09-21 02:25:41 +0200

    Spadające "Xd"

---
+++ PodstępneHobbity/Assets/Scripts/RockSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawn : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] Edges;
+    [SerializeField]
+    GameObject Rock;
+    [SerializeField]
+    float MaxDely = 3, MinDely = 1;
+
+    float time = 0;
+    float NextDely;
+
+    void Start()
+    {
+        NextDely = MaxDely;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > NextDely)
+        {
+            Spawn();
+            NextDely = Random.value * (MaxDely - MinDely) + MinDely;
+            time = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject NewRock = Instantiate(Rock, gameObject.transform);
+        float XPozytion = Random.value * (Edges[1].position.x - Edges[0].position.x) + Edges[0].position.x;
+        float YPozytion = Random.value * (Edges[1].position.y - Edges[0].position.y) + Edges[0].position.y;
+        NewRock.transform.position = new Vector2(XPozytion, YPozytion);
+    }
+
+}
