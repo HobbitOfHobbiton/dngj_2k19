9c006943bb2f910a522e4f7a69d98c2b933ece55
GroveW <qswdw3ed3@gmail.com>
2019-09-21 09:04:23 +0200

    no message

---
+++ PodstępneHobbity/Assets/PlayerAffection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAffection : MonoBehaviour
+{
+    private SpriteRenderer rn;
+
+    private bool isProtected = false;
+    private bool isUnderLight = false;
+
+    void Start()
+    {
+        rn = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "SauronLight" && this.name == "Frodo")
+        {
+            isUnderLight = true;
+
+            if (!isProtected)
+            {
+                GetAffectedByEyeLight();
+            }
+        }
+
+        if (col.gameObject.tag == "ProtectingBoulder" && this.name == "Frodo")
+        {
+            isProtected = true;
+            rn.color = new Color(1f, 1f, 1f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "SauronLight" && this.name == "Frodo")
+        {
+            rn.color = new Color(1f, 1f, 1f);
+            isUnderLight = false;
+        }
+
+        if (col.gameObject.tag == "ProtectingBoulder" && this.name == "Frodo")
+        {
+            isProtected = false;
+
+            if (isUnderLight)
+            {
+                GetAffectedByEyeLight();
+            }
+        }
+    }
+
+    void GetAffectedByEyeLight()
+    {
+        rn.color = new Color(100, 0, 0);
+    }
+}
---
+++ PodstępneHobbity/Assets/Scripts/EyeLightBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLightBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private Transform cameraTransform;
+    [SerializeField]
+    private float distanceFromCamera = 100f;
+
+    private float delayTime = 0f;
+    private float finishedTimeStamp;
+
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        delayTime = Random.Range(1, 10);
+        finishedTimeStamp = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving && Time.time >= finishedTimeStamp + delayTime )
+        {
+            GoToStartPos();
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+            if (transform.position.x - cameraTransform.position.x >= distanceFromCamera)
+            {
+                isMoving = false;
+                delayTime = Random.Range(1, 10);
+                finishedTimeStamp = Time.time;
+            }
+        }
+    }
+
+    void GoToStartPos()
+    {
+        Vector3 leftEdgePos = Camera.main.ScreenToViewportPoint(new Vector3(0, Camera.main.pixelHeight, 0));
+        Debug.Log(leftEdgePos);
+        transform.position = new Vector3(leftEdgePos.x, transform.position.y, transform.position.z);
+    }
+}
