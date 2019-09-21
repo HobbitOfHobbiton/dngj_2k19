495413b66adc408c110601d20ec0c0a5b7bbe4c0
piechu007 <55602442+piechu007@users.noreply.github.com>
2019-09-21 12:43:18 +0200

    Pasek Staminy

---
+++ PodstępneHobbity/Assets/Scripts/BarOfStamina.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarOfStamina : MonoBehaviour
+{
+    [SerializeField]
+    Transform Yelow;
+
+    float StartScale, EdgeOfBar;
+
+    void Start()
+    {
+        StartScale = Yelow.localScale.x;
+        EdgeOfBar = StartScale / (-200);
+    }
+
+    public void SetHp(float Stamina, float MaxStamina)
+    {
+        Yelow.localScale = new Vector2(StartScale * (Stamina / MaxStamina), Yelow.localScale.y);
+        Yelow.localPosition = new Vector3(EdgeOfBar * ((MaxStamina - Stamina) / MaxStamina), Yelow.localPosition.y, -1);
+    }
+}
---
+++ PodstępneHobbity/Assets/Scripts/PlayerMovement/StaminaScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaScript : MonoBehaviour
+{
+    [SerializeField]
+    int Stamina = 100;
+    [SerializeField]
+    BarOfHp barOfStamina;
+
+    int MaxStamina;
+
+    void Start()
+    {
+        MaxStamina = Stamina;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoseStamina(int HowMany)
+    {
+        Stamina -= HowMany;
+        if (Stamina <= 0)
+            EndOfStamina();
+    }
+
+    void EndOfStamina()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, 90);
+    }
+}
