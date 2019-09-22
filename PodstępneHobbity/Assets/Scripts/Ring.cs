204e55dd06bd8c2449d7312bbc2189da87b4da5e
piechu007 <55602442+piechu007@users.noreply.github.com>
2019-09-22 12:17:16 +0200

    Podnoszenie Pierścienia

---
+++ PodstępneHobbity/Assets/Scripts/Ring.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    [SerializeField]
+    Transform Owner;
+
+    SpriteRenderer sr;
+    BoxCollider2D col;
+
+
+    void Start()
+    {
+        Owner = GameObject.Find("Frodo").transform;
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (Owner == null)
+        {
+            sr.enabled = true;
+            col.enabled = true;
+        }
+        else
+        {
+            transform.position = Owner.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Owner = collision.transform;
+            sr.enabled = false;
+            col.enabled = false;
+        }
+    }
+}
