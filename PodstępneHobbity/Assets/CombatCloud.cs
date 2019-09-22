0f97521f5179cbe9f99c4e06974cf32b68d4586d
GroveW <qswdw3ed3@gmail.com>
2019-09-22 13:54:18 +0200

    Dymki przy kombacie

---
+++ PodstępneHobbity/Assets/CombatCloud.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatCloud : MonoBehaviour
+{
+    [SerializeField]
+    private float timeToLive = 1f;
+
+    private float bornTimeStamp;
+
+    private SpriteRenderer rn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bornTimeStamp = Time.time;
+        rn = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rn.enabled && Time.time - bornTimeStamp >= timeToLive)
+        {
+            rn.enabled = false;
+        }
+    }
+
+    public void Show()
+    {
+        float randomXPosMin = Camera.main.ViewportToWorldPoint(new Vector3(0, 0)).x;
+        float randomXPosMax = Camera.main.ViewportToWorldPoint(new Vector3(1, 0)).x;
+        float randomYPosMin = Camera.main.ViewportToWorldPoint(new Vector3(0, 0)).y;
+        float randomYPosMax = Camera.main.ViewportToWorldPoint(new Vector3(0, 1)).y;
+
+        float randomXPos = Random.Range(randomXPosMin, randomXPosMax);
+        float randomYPos = Random.Range(randomYPosMin, randomYPosMax);
+        transform.position = new Vector3(randomXPos, randomYPos, transform.position.z);
+        rn.enabled = true;
+        bornTimeStamp = Time.time;
+    }
+}
--- PodstępneHobbity/Assets/Scripts/PlayerMovement/health.cs
+++ PodstępneHobbity/Assets/Scripts/PlayerMovement/health.cs
@@ -8,12 +8,29 @@ public class health : MonoBehaviour
     int hp = 100 ;
     [SerializeField]
     BarOfHp barOfHp;
+    [SerializeField]
+    private CombatCloud[] combatCloudsToStatify;
+    [SerializeField]
+    private SpriteRenderer[] combatCloudRensToStatify;
+
+    private static CombatCloud[] combatClouds;
+    private static SpriteRenderer[] combatCloudRens;
 
     int MaxHp;
 
     void Start()
     {
         MaxHp = hp;
+
+        if (combatCloudsToStatify != null)
+        {
+            combatClouds = combatCloudsToStatify;
+        }
+
+        if (combatCloudRensToStatify != null)
+        {
+            combatCloudRens = combatCloudRensToStatify;
+        }
     }
 
     public void demage(int power)
@@ -23,6 +40,8 @@ public class health : MonoBehaviour
         barOfHp.SetHp(hp, MaxHp);
         if (hp <= 0)
             Death();
+        
+        ShowCombatCloud();
     }
 
     public void damageOrc(int power)
@@ -31,6 +50,8 @@ public class health : MonoBehaviour
         barOfHp.SetHp(hp, MaxHp);
         if (hp <= 0)
             DeathOrc();
+
+        ShowCombatCloud();
     }
 
     void Death()
@@ -42,5 +63,27 @@ public class health : MonoBehaviour
     void DeathOrc()
     {
         Destroy(gameObject);
+    }
+
+    void ShowCombatCloud()
+    {
+        int randomCloudIndex = Random.Range(0, combatClouds.Length);
+        if (!combatCloudRens[randomCloudIndex].enabled)
+        {
+            combatClouds[randomCloudIndex].Show();
+        }
+        else
+        {
+            foreach(SpriteRenderer ren in combatCloudRens)
+            {
+                int unusedIndex = 0;
+                if (!ren.enabled)
+                {
+                    combatClouds[unusedIndex].Show();
+                    break;
+                }
+                unusedIndex++;
+            }
+        }
     }
 }
